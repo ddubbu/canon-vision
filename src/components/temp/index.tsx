5e94abc85aa2a2a636d2c9890b8fe57f68a09eaa
ddubbu <hello.ddubbu@gmail.com>
2023-12-20 21:14:52 +0800

    feat: Downgrade next 14 > 12, add styled component

---
+++ src/components/temp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as Styled from './index.styled';
+
+const XComponent: React.FC = () => {
+	return (
+		<Styled.Wrapper>
+			<h1>Test</h1>
+		</Styled.Wrapper>
+	);
+};
+
+export default XComponent;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Temp from '@/components/temp';
+
+export default function Home() {
+	return <Temp />;
+}
