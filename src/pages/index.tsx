47f6120abc93b3ce590d66b9ae7574bcc117d2d5
ddubbu <hello.ddubbu@gmail.com>
2023-12-26 00:24:46 +0800

    feat: 메타데이터 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import { GlobalStyle } from '@/style/global.styled';
 import { Global } from '@emotion/react';
-import { NextComponentType } from 'next';
-import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import * as LayoutStyled from '@/components/layout/index.styled';
 import GNB from '@/components/layout/GNB';
 import Home from '@/components/home';
 import Profile from '@/components/profile';
+import Head from 'next/head';
 
 type PageType = 'home' | 'profile';
 
-const Index: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Component, pageProps }) => {
+const Index = () => {
 	const [type, setType] = useState<PageType>('profile');
 
 	const handleClickLogo = () => {
@@ -23,7 +22,13 @@ const Index: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Compo
 
 	return (
 		<>
-			<link rel="stylesheet" href="https://use.typekit.net/aga0rpu.css"></link>
+			<Head>
+				<title>canon vision</title>
+				<link rel="icon" href="/assets/common/favicon.jpg" />
+				<meta name="description" content="an architecture practice" />{' '}
+				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
+				<link rel="stylesheet" href="https://use.typekit.net/aga0rpu.css"></link>
+			</Head>
 			<Global styles={GlobalStyle} />
 			<LayoutStyled.Container>
 				<GNB handleClickLogo={handleClickLogo} />
