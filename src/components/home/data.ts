98e0b44d308c6065186aaf2f47ad33e9e3fc44b5
ddubbu <hello.ddubbu@gmail.com>
2024-01-02 19:12:23 +0800

    feat: Add LoungeY Project images

--- src/components/home/data.ts
+++ src/components/home/data.ts
@@ -11,7 +11,28 @@ const PAVILION_S: Project = {
 	],
 };
 
+const LOUNGE_Y_BASEURL = '/assets/project/Lounge_Y';
+const Lounge_Y: Project = {
+	imageList: [
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_1.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_2.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_3.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_4.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_5.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_6.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_7.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_8.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_9.jpg`,
+	],
+	draftList: [
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_1.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_2.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_3.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_4.jpg`,
+	],
+};
+
 // TODO: 프로젝트 늘어나면, 추가할 곳
-const PROJECT_DATA: Project[] = [PAVILION_S];
+const PROJECT_DATA: Project[] = [PAVILION_S, Lounge_Y];
 
 export default PROJECT_DATA;
