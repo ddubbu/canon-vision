e7c9f91e0ddf6e31b291d2b8c9ed38bf4af6f00d
ddubbu <hello.ddubbu@gmail.com>
2024-01-05 11:02:27 +0800

    chore: 이미지 용량 최적화

--- src/components/home/data.ts
+++ src/components/home/data.ts
@@ -2,33 +2,36 @@ import { Project } from './type';
 
 const PAVILION_S_BASEURL = '/assets/project/Pavilion_S';
 const PAVILION_S: Project = {
-	imageList: [`${PAVILION_S_BASEURL}/Pavilion S_i_1.jpg`],
+	imageList: [`${PAVILION_S_BASEURL}/Pavilion S_i_1.v240105.jpg`],
 	draftList: [
-		`${PAVILION_S_BASEURL}/Pavilion S_d_1.jpg`,
-		`${PAVILION_S_BASEURL}/Pavilion S_d_2.jpg`,
-		`${PAVILION_S_BASEURL}/Pavilion S_d_3.jpg`,
-		`${PAVILION_S_BASEURL}/Pavilion S_d_4.png`,
+		`${PAVILION_S_BASEURL}/Pavilion S_d_1.v240105.jpg`,
+		`${PAVILION_S_BASEURL}/Pavilion S_d_2.v240105.jpg`,
+		`${PAVILION_S_BASEURL}/Pavilion S_d_3.v240105.jpg`,
+		`${PAVILION_S_BASEURL}/Pavilion S_d_4.v240105.jpg`,
 	],
 };
 
 const LOUNGE_Y_BASEURL = '/assets/project/Lounge_Y';
 const Lounge_Y: Project = {
 	imageList: [
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_1.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_2.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_3.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_4.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_5.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_6.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_7.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_8.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_left_9.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_1.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_2.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_3.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_4.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_5.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_6.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_7.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_8.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_left_9.v240105.jpg`,
 	],
 	draftList: [
-		`${LOUNGE_Y_BASEURL}/lounge_Y_right_1.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_right_2.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_right_3.jpg`,
-		`${LOUNGE_Y_BASEURL}/lounge_Y_right_4.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_1.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_2.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_3.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_4.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_5.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_6.v240105.jpg`,
+		`${LOUNGE_Y_BASEURL}/lounge_Y_right_7.v240105.jpg`,
 	],
 };
 
